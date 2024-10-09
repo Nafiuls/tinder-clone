7b24acd35c25c57507e22963d4403e521b4f3902
Nafiuls <snafiul700@gmail.com>
2024-10-09 18:46:57 +0600

    first commit

---
+++ api/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+//routes
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import matchesRoutes from './routes/matchesRoutes.js';
+import messageRoutes from './routes/messageRoutes.js';
+
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/matches', matchesRoutes);
+app.use('/api/messages', messageRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
